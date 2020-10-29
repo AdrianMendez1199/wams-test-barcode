cc195f2208cf4279123458dfcc120a8c6f88cc3d
Adrian Mendez <amendez@viva.com.do>
2020-10-29 13:21:41 -0400

    initial commit

---
+++ docs/precache-manifest.ce6d1fd8bcc55c91550a12f6489b649c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22ad4bbda58a849e7f85f3f8f08370b",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "e64b22836ba8a50a3555",
+    "url": "/demo/static/js/2.2df93ef4.chunk.js"
+  },
+  {
+    "revision": "81fc10a5a850820b9b22",
+    "url": "/demo/static/js/main.44837147.chunk.js"
+  },
+  {
+    "revision": "7ac8627d350d3011a1f2",
+    "url": "/demo/static/js/runtime-main.43a555ef.js"
+  },
+  {
+    "revision": "b946c8e627a45da7bbf33016aedb1ade",
+    "url": "/demo/static/media/zbar.wasm.b946c8e6.bin"
+  }
+]);
